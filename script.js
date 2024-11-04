a50cb6e94601c9737a1a335dc1279c63e9c1c939
Irfan Fadilah <hello@irfanfadilah.com>
2024-11-05 05:41:21 +0700

    Improve Script

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const textarea = document.getElementsByTagName('textarea')[0]
-  textarea.value = window.localStorage.getItem('data')
+const textarea = document.getElementsByTagName('textarea')[0]
+textarea.value = window.localStorage.getItem('data')
 
-  textarea.addEventListener("blur", function() {
-    window.localStorage.setItem('data', this.value)
-  });
-});
+textarea.addEventListener("blur", function() {
+  window.localStorage.setItem('data', this.value)
+})
